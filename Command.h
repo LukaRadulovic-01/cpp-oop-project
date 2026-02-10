fc9489565c038d7a1b3de3b6f0f8fbbc7e146296
LukaRadulovic-01 <luka.radulovic145@gmail.com>
2026-02-10 15:18:39 +0100

    Delete Command.h

--- Command.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include "InStream.h"
-#include "OutStream.h"
-
-class Command {
-public:
-    virtual ~Command() = default;
-    virtual void execute(InStream& in, OutStream& out) = 0;
-};
